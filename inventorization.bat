aee6ce1fd04a304c37e0603a8505721f34e03e2a
Ground-Zerro <ksa65s571dozor@gmail.com>
2024-05-01 11:09:57 +1100

    Update inventorization.bat

--- inventorization.bat
+++ inventorization.bat
@@ -1,9 +1,13 @@
-@echo on
+@echo off
 setlocal enabledelayedexpansion
 chcp 65001 > nul
 set OUTPUT_FILE=%COMPUTERNAME%.txt
 del /q /f %OUTPUT_FILE%
-systeminfo | findstr /v /c:"OS Manufacturer" /c:"OS Configuration" /c:"OS Build Type" /c:"Registered Owner" /c:"Product ID" /c:"System Directory" /c:"Boot Device" /c:"System Locale" /c:"Input Locale" /c:"Available Physical Memory" /c:"Virtual Memory: Max Size" /c:"Virtual Memory: Available" /c:"Virtual Memory: In Use" /c:"Page File Location(s)" /c:"Hyper-V Requirements:" /c:"Virtualization Enabled In Firmware" /c:"Second Level Address Translation" /c:"Data Execution Prevention Available"> %OUTPUT_FILE%
+
+echo Выполненно от имени:> %OUTPUT_FILE%
+whoami>> %OUTPUT_FILE%
+
+systeminfo | findstr /v /c:"OS Manufacturer" /c:"OS Configuration" /c:"OS Build Type" /c:"Registered Owner" /c:"Product ID" /c:"System Directory" /c:"Boot Device" /c:"System Locale" /c:"Input Locale" /c:"Available Physical Memory" /c:"Virtual Memory: Max Size" /c:"Virtual Memory: Available" /c:"Virtual Memory: In Use" /c:"Page File Location(s)" /c:"Hyper-V Requirements:" /c:"Virtualization Enabled In Firmware" /c:"Second Level Address Translation" /c:"Data Execution Prevention Available">> %OUTPUT_FILE%
 
 echo.>> %OUTPUT_FILE%
 echo Процессор:>> %OUTPUT_FILE%
@@ -25,16 +29,23 @@ wmic diskdrive get model, size, interfacetype, serialnumber | findstr /v /r "^$"
 echo.>> %OUTPUT_FILE%
 echo CD/DVD приводы:>> %OUTPUT_FILE%
 wmic cdrom get caption, name | findstr /v /r "^$">> %OUTPUT_FILE%
-if %ERRORLEVEL% neq 0 (
-    echo отсутствуют>> %OUTPUT_FILE%
-)
+if %ERRORLEVEL% neq 0 (echo отсутствуют)>> %OUTPUT_FILE%
 
 echo.>> %OUTPUT_FILE%
 echo Монитор:>> %OUTPUT_FILE%
 powershell -command "$OUTPUT_FILE='%OUTPUT_FILE%'; function Decode { If ($args[0] -is [System.Array]) { [System.Text.Encoding]::ASCII.GetString($args[0]) } Else { 'Not Found' } } ForEach ($Monitor in Get-WmiObject WmiMonitorID -Namespace root\wmi) { $Manufacturer = Decode $Monitor.ManufacturerName -notmatch 0; $Name = Decode $Monitor.UserFriendlyName -notmatch 0; $Serial = Decode $Monitor.SerialNumberID -notmatch 0; Add-Content -Path $OUTPUT_FILE -Value ('Manufacturer: ' + $Manufacturer + ' Name: ' + $Name + ' Serial Number: ' + $Serial); }"
 
-
 echo.>> %OUTPUT_FILE%
 echo Пользователи:>> %OUTPUT_FILE%
 set "user_path=C:\Users\"
 for /d %%i in ("%user_path%*") do @(if exist "%%i\AppData" (echo %%~nxi>> %OUTPUT_FILE%))
+
+echo.>> %OUTPUT_FILE%
+echo Принтеры:>> %OUTPUT_FILE%
+wmic printer get name, deviceID, PortName, Network, local>> %OUTPUT_FILE%
+if %ERRORLEVEL% neq 0 (echo не обнаружено)>> %OUTPUT_FILE%
+
+echo.>> %OUTPUT_FILE%
+echo Сканеры:>> %OUTPUT_FILE%
+wmic scanner get name, deviceid>> %OUTPUT_FILE%
+if %ERRORLEVEL% neq 0 (echo не обнаружено)>> %OUTPUT_FILE%
