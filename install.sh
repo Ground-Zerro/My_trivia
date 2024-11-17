de76a4bff07b890aad8e74b989fb294c69cb21f8
Ground-Zerro <ksa65s571dozor@gmail.com>
2024-11-17 12:15:31 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,14 +3,14 @@
 SCRIPT="work.sh"
 TMP_DIR="/tmp"
 
-# Удаляем файл, если он существует
+# Удаляем старую версию скрипта, если она существует
 rm -f "$TMP_DIR/$SCRIPT"
 
-# Скачиваем новый файл
-curl -L -s "https://raw.githubusercontent.com/Ground-Zerro/My_trivia/refs/heads/main/$SCRIPT" --output "$TMP_DIR/$SCRIPT"
+# Скачиваем новый скрипт
+curl -sL "https://raw.githubusercontent.com/Ground-Zerro/My_trivia/refs/heads/main/$SCRIPT" --output "$TMP_DIR/$SCRIPT"
 
 # Назначаем права на выполнение
 chmod +x "$TMP_DIR/$SCRIPT"
 
-# Выполняем скрипт
-"$TMP_DIR/$SCRIPT"
+# Выполняем скрипт в интерактивном режиме
+bash -i "$TMP_DIR/$SCRIPT"
